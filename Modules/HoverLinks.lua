e365c066ef4a05307ef01ae7a7227fb6eacdb5c8
Adam Koch <akoch@southsidechristian.org>
2020-01-23 09:15:45 -0500

    Completed Hover Links module.

--- Modules/HoverLinks.lua
+++ Modules/HoverLinks.lua
@@ -2,15 +2,60 @@
 -- ElvUI Chat Tweaks By Crackpotx (US, Lightbringer)
 -- Based on functionality provided by Prat and/or Chatter
 -------------------------------------------------------------------------------
-local Module = ElvUI_ChatTweaks:NewModule("Hover Links", "AceEvent-3.0", "AceHook-3.0", "AceTimer-3.0", "AceConsole-3.0")
+local Module = ElvUI_ChatTweaks:NewModule("Hover Links", "AceHook-3.0")
 local L = LibStub("AceLocale-3.0"):GetLocale("ElvUI_ChatTweaks", false)
 Module.name = L["Hover Links"]
 Module.namespace = string.gsub(Module.name, " ", "")
+Module.showingTip = false
 
-function Module:OnInitialize()
-	self.db = ElvUI_ChatTweaks.db:RegisterNamespace(Module.namespace, defaults)
-	db = self.db.global
-	self.debug = ElvUI_ChatTweaks.db.global.debugging
+local match = string.match
+
+-- the link types we'll interract with
+local linkTypes = {
+	item = true,
+	enchant = true,
+	spell = true,
+	quest = true,
+	achievement = true,
+	currency = true
+}
+
+function Module:OnHyperlinkEnter(frame, link)
+	-- get the link type using regex
+	local lType = match(link, "^(.-):")
+	
+	if linkTypes[lType] then
+		-- show the tooltip
+		self.showingTip = true
+		ShowUIPanel(GameTooltip)
+		GameTooltip:SetOwner(E.UIParent, "ANCHOR_CURSOR")
+		GameTooltip:SetHyperlink(link)
+		GameTooltip:Show()
+	end
+end
+
+function Module:OnHyperlinkLeave(frame, link)
+	if self.showingTip then
+		-- hide the tooltip
+		self.showingTip = false
+		GameTooltip:Hide()
+	end
+end
+
+function Module:OnEnable()
+	for i = 1, NUM_CHAT_WINDOWS do
+		local Frame = _G["ChatFrame" .. i]
+		self:HookScript(Frame, "OnHyperlinkEnter", OnHyperlinkEnter)
+		self:HookScript(Frame, "OnHyperlinkLeave", OnHyperlinkLeave)
+	end
+end
+
+function Module:OnDisable()
+	for i = 1, NUM_CHAT_WINDOWS do
+		local Frame = _G["ChatFrame" .. i]
+		self:Unhook(Frame, "OnHyperlinkEnter")
+		self:Unhook(Frame, "OnHyperlinkLeave")
+	end
 end
 
 function Module:Info()
--- Modules/SemiAutoCongratulate.lua
+++ Modules/SemiAutoCongratulate.lua
@@ -14,6 +14,55 @@ local UnitName = _G["UnitName"]
 local UnitInParty = _G["UnitInParty"]
 local UnitInRaid = _G["UnitInRaid"]
 
+local db, options
+local defaults = {
+	global = {
+		afk = true,
+		dnd = true,
+		nearby = false,
+		guild = true,
+		party = false,
+		raid = false
+	}
+}
+
+function Module:Achievement(event, message, sender)
+	local temp = {strsplit("-", sender)}
+	local senderName = temp[1]
+
+	-- don't run if afk/dnd and setting is enabled
+	if db.afk and UnitIsAFK("player") then return end
+	if db.dnd and UnitIsDND("player") then return end
+
+	-- pull the achievement id from the message
+	local id = message:match("|cffffff00|Hachievement:([0-9]+):.+:[%-0-9]+:[%-0-9]+:[%-0-9]+:[%-0-9]+:[%-0-9]+:[%-0-9]+:[%-0-9]+:[%-0-9]+|h%[[^]]+%]|h|r")
+	
+	if event == "CHAT_MSG_ACHIEVEMENT" then
+		-- nearby
+		if db.nearby and not UnitInParty(sender) and not UnitInRaid(sender) then
+		
+		-- party
+		elseif db.party and UnitInParty(sender) and not UnitInRaid(sender) then
+
+		-- raid
+		elseif db.raid and UnitInRaid(sender) then
+
+		end
+	elseif event == "CHAT_MSG_GUILD_ACHIEVEMENT" and db.guild then
+
+	end
+end
+
+function Module:OnEnable()
+	self:RegisterEvent("CHAT_MSG_ACHIEVEMENT", "Achievement")
+	self:RegisterEvent("CHAT_MSG_GUILD_ACHIEVEMENT", "Achievement")
+end
+
+function Module:OnDisable()
+	self:UnregisterEvent("CHAT_MSG_ACHIEVEMENT")
+	self:UnregisterEvent("CHAT_MSG_GUILD_ACHIEVEMENT")
+end
+
 function Module:OnInitialize()
 	self.db = ElvUI_ChatTweaks.db:RegisterNamespace(Module.namespace, defaults)
 	db = self.db.global
@@ -22,4 +71,72 @@ end
 
 function Module:Info()
 	return L["Prints a link you can click to congratulate when someone obtains an achievement."]
+end
+
+function Module:GetOptions()
+	if not options then
+		options = {
+			disableIf = {
+				type = "group",
+				order = 98,
+				name = L["Disable if..."],
+				guiInline = true,
+				args = {
+					afk = {
+						type = "toggle",
+						order = 1,
+						name = L["AFK"],
+						desc = L["Disable while you're AFK flagged."],
+						get = function() return db.afk end,
+						set = function(_, value) db.afk = value end,
+					},
+					dnd = {
+						type = "toggle",
+						order = 2,
+						name = L["Busy"],
+						desc = L["Disable while you're DND flagged."],
+						get = function() return db.dnd end,
+						set = function(_, value) db.dnd = value end,
+					},
+				}
+			},
+			whoToGrts = {
+				type = "group",
+				order = 99,
+				name = L["Who to Congratulate?"],
+				guiInline = true,
+				args = {
+					nearby = {
+						type = "toggle",
+						name = L["Nearby People"],
+						desc = L["Congratulate achievements earned by people near you."],
+						get = function() return db.nearby end,
+						set = function(_, value) db.nearby = value end
+					},
+					raid = {
+						type = "toggle",
+						name = L["Raid Members"],
+						desc = L["Congratulate achievements earned by people in your raid."],
+						get = function() return db.raid end,
+						set = function(_, value) db.raid = value end,
+					},
+					guild = {
+						type = "toggle",
+						name = L["Guildmates"],
+						desc = L["Congratulate achievements earned by guildmates."],
+						get = function() return db.guild end,
+						set = function(_, value) db.guild = value end,
+					},
+					party = {
+						type = "toggle",
+						name = L["Party Members"],
+						desc = L["Congratulate achievements earned by people in your party."],
+						get = function() return db.party end,
+						set = function(_, value) db.party = value end,
+					},
+				},
+			},
+		}
+	end
+	return options
 end
